3ad2111c01a97a08bbf1d8ba245b7c4f0523c5a4
Julien Viet <julien@julienviet.com>
2023-06-20 10:31:55 +0200

    Update to vertx 5

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -11,7 +11,8 @@ public class MainVerticle extends AbstractVerticle {
       req.response()
         .putHeader("content-type", "text/plain")
         .end("Hello from Vert.x!");
-    }).listen(8080, http -> {
+    }).listen(8080)
+      .onComplete(http -> {
       if (http.succeeded()) {
         startPromise.complete();
         System.out.println("HTTP server started on port 8080");
