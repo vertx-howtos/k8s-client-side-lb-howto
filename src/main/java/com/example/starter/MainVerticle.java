1747501dbc279ee23d44ef8da50ac0276415d0d9
Julien Viet <julien@julienviet.com>
2023-09-27 07:58:46 +0200

    Up

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -6,10 +6,11 @@ import io.vertx.core.Promise;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.*;
 import io.vertx.core.json.JsonObject;
+import io.vertx.serviceresolver.ServiceAddress;
+import io.vertx.serviceresolver.kube.KubeResolver;
+import io.vertx.serviceresolver.kube.KubeResolverOptions;
 
 import java.io.File;
-import java.util.Base64;
-import java.util.Map;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -22,46 +23,53 @@ public class MainVerticle extends AbstractVerticle {
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
 
-    String host = System.getenv(KUBERNETES_SERVICE_HOST);
-    String port = System.getenv(KUBERNETES_SERVICE_PORT);
-    File tokenFile = new File(KUBERNETES_SERVICE_ACCOUNT_TOKEN);
-    File ca = new File(KUBERNETES_SERVICE_ACCOUNT_CA);
-    Buffer token;
-    JsonObject jwt;
-    if (tokenFile.exists()) {
-      token = vertx.fileSystem().readFileBlocking(KUBERNETES_SERVICE_ACCOUNT_TOKEN);
-//      byte[] decoded = Base64.getDecoder().decode(token.toString());
-//      jwt = new JsonObject(Buffer.buffer(decoded));
-      jwt = null;
-    } else {
-      token = null;
-      jwt = null;
-    }
+//    String host = System.getenv(KUBERNETES_SERVICE_HOST);
+//    String port = System.getenv(KUBERNETES_SERVICE_PORT);
+//    File tokenFile = new File(KUBERNETES_SERVICE_ACCOUNT_TOKEN);
+//    File ca = new File(KUBERNETES_SERVICE_ACCOUNT_CA);
+//    Buffer token;
+//    if (tokenFile.exists()) {
+//      token = vertx.fileSystem().readFileBlocking(KUBERNETES_SERVICE_ACCOUNT_TOKEN);
+//    } else {
+//      token = null;
+//    }
 
-    client = vertx.createHttpClient(new HttpClientOptions()
-      .setSsl(true)
-      .setTrustAll(true));
+    client = vertx
+      .httpClientBuilder()
+      .withAddressResolver(KubeResolver.create(vertx, new KubeResolverOptions()
+//        .setNamespace("default")
+//        .setHost(host)
+//        .setPort(Integer.parseInt(port))
+//        .setHttpClientOptions(new HttpClientOptions().setSsl(true).setTrustAll(true))
+//        .setWebSocketClientOptions(new WebSocketClientOptions().setSsl(true).setTrustAll(true))
+//        .setBearerToken(token.toString())
+        )
+      )
+      .build();
+
+    ServiceAddress sa = ServiceAddress.create("hello-node");
+
     vertx.createHttpServer().requestHandler(req -> {
-
-        Future<HttpClientRequest> fut = client.request(HttpMethod.GET, Integer.parseInt(port), host, "/api/v1/namespaces/" + "default" + "/endpoints");
-
-        fut.compose(r -> {
-          if (token != null) {
-            r.putHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+        RequestOptions connect = new RequestOptions()
+          .setMethod(HttpMethod.GET)
+          .setServer(sa)
+          .setHost("localhost")
+          .setPort(80)
+          .setURI("/");
+        Future<HttpClientRequest> fut = client.request(connect);
+        fut.compose(r -> r.send()
+          .compose(resp -> resp.body()
+            .map(body -> "Hello from: " + r.connection().remoteAddress() + " with: " + body)))
+          .onComplete(ar -> {
+          if (ar.succeeded()) {
+            req.response()
+              .putHeader("content-type", "text/plain")
+              .end(ar.result());
+          } else {
+            req.response()
+              .putHeader("content-type", "text/plain")
+              .end("Error: " + ar.cause().getMessage());
           }
-          return r.send().compose(HttpClientResponse::body);
-        }).onComplete(ar -> {
-          StringBuilder sb = new StringBuilder();
-          Map<String, String> env = System.getenv();
-          env.forEach((key, value) -> sb.append(key).append(":").append(value).append("\r\n"));
-
-          req.response()
-            .putHeader("content-type", "text/plain")
-            .end("Hello from Vert.x!" +
-              "\r\n" + token +
-              "\r\n" + jwt +
-              "\r\n" + ar.result().toJsonObject().encodePrettily() +
-              "\r\n" + sb);
         });
 
       }).listen(8080)
