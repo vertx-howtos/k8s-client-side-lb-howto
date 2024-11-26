706cfd9faed0d720e85e3051d8bcb9a0a4288e00
Julien Viet <julien@julienviet.com>
2024-11-26 14:51:26 +0100

    Up

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -1,19 +1,20 @@
 package com.example.starter;
 
-import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
-import io.vertx.core.Promise;
+import io.vertx.core.VerticleBase;
 import io.vertx.core.http.*;
 import io.vertx.serviceresolver.ServiceAddress;
 import io.vertx.serviceresolver.kube.KubeResolver;
 import io.vertx.serviceresolver.kube.KubeResolverOptions;
 
-public class MainVerticle extends AbstractVerticle {
+public class MainVerticle extends VerticleBase {
+
+  private static final ServiceAddress SERCICE_ADDRESS = ServiceAddress.of("hello-node");
 
   private HttpClient client;
 
   @Override
-  public void start(Promise<Void> startPromise) throws Exception {
+  public Future<?> start() {
 
     client = vertx
       .httpClientBuilder()
@@ -21,44 +22,35 @@ public class MainVerticle extends AbstractVerticle {
       )
       .build();
 
-    ServiceAddress sa = ServiceAddress.of("hello-node");
-
     StringBuilder sb = new StringBuilder();
     System.getenv().forEach((k, v) -> {
       sb.append(k).append("=").append(v).append("\n");
     });
 
-    vertx.createHttpServer().requestHandler(req -> {
+    return vertx.createHttpServer()
+      .requestHandler(req -> {
         RequestOptions connect = new RequestOptions()
           .setMethod(HttpMethod.GET)
-          .setServer(sa)
+          .setServer(SERCICE_ADDRESS)
           .setHost("localhost")
           .setPort(80)
           .setURI("/");
         Future<HttpClientRequest> fut = client.request(connect);
         fut.compose(r -> r.send()
-          .compose(resp -> resp.body()
-            .map(body -> "Hello from: " + r.connection().remoteAddress() + " with: " + body + "\n" + sb)))
-          .onComplete(ar -> {
-          if (ar.succeeded()) {
+            .compose(resp -> resp.body()
+              .map(body -> "Hello from: " + r.connection().remoteAddress() + " with: " + body + "\n" + sb)))
+          .onSuccess(res -> {
             req.response()
               .putHeader("content-type", "text/plain")
-              .end(ar.result());
-          } else {
+              .end(res);
+          })
+          .onFailure(cause -> {
             req.response()
+              .setStatusCode(500)
               .putHeader("content-type", "text/plain")
-              .end("Error: " + ar.cause().getMessage());
-          }
-        });
+              .end("Error: " + cause.getMessage());
+          });
 
-      }).listen(8080)
-      .onComplete(http -> {
-        if (http.succeeded()) {
-          startPromise.complete();
-          System.out.println("HTTP server started on port 8080");
-        } else {
-          startPromise.fail(http.cause());
-        }
-      });
+      }).listen(8080);
   }
 }
